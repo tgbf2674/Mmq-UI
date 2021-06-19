3647861a1e9b0c30f3295ea66ac27e0c24acaa47
maqing <136518041@qq.com>
2021-06-19 23:38:34 +0800

    init

---
+++ vue3-ui/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>hello</div>
+  <frank></frank>
+</template>
+
+<script>
+import frank from './components/frank.vue'
+export default {
+  name: 'App',
+  components: {
+    frank
+  }
+}
+</script>
---
+++ vue3-ui/src/components/frank.vue
@@ -0,0 +1,13 @@
+<template>
+ <div>asd</div>
+</template>
+
+<script>
+export default {
+name: "frank"
+}
+</script>
+
+<style scoped>
+
+</style>
