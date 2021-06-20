f63eee641d3b32e7a89d30142d5f9194525e4b45
maqing <136518041@qq.com>
2021-06-20 22:56:18 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>hello | <router-link to="/">frank</router-link> | <router-link to="/xxx">frank</router-link></div>
+  <router-view></router-view>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+}
+</script>
---
+++ src/components/frank2.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    woshi2
+  </div>
+</template>
+
+<script>
+export default {
+name: "frank2"
+}
+</script>
+
+<style scoped>
+
+</style>
