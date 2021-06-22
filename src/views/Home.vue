fe8b1d128a49d8ce5439356d55ab33c36f63917a
maqing <136518041@qq.com>
2021-06-23 00:41:25 +0800

    å背景渐变

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="topNavAndBanner">
    <topNav />
     <div class="banner">
       <h1>vue3UI</h1>
@@ -23,13 +23,15 @@ name: "Home",
 </script>
 
 <style lang="scss" scoped>
+.topNavAndBanner{
+  background: linear-gradient(145deg,rgba(227,255,253,1) 0%, rgba(183,233,230,1) 100%);
+}
 .banner {
   padding: 100px 0;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  background: lightgreen;
   > .actions{
     padding: 8px 0;
     a {
