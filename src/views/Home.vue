8050e15f9ed4b07a84893916fe0413980b3a9c30
maqing <136518041@qq.com>
2021-06-23 19:40:30 +0800

    引入iconfont

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,6 +10,17 @@
       </p>
     </div>
   </div>
+  <div class="features">
+  <svg class="icon">
+    <use xlink:href="#icon-Vue"></use>
+  </svg>
+  <svg class="icon">
+    <use xlink:href="#icon-ts"></use>
+  </svg>
+  <svg class="icon">
+    <use xlink:href="#icon-LC_icon_light_line"></use>
+  </svg>
+  </div>
 </template>
 
 <script lang="ts">
@@ -23,10 +34,19 @@ name: "Home",
 </script>
 
 <style lang="scss" scoped>
+$green: #02bcb0;
+$border-radius: 4px;
 .topNavAndBanner{
   background: linear-gradient(145deg,rgba(227,255,253,1) 0%, rgba(183,233,230,1) 100%);
 }
+.features{
+  >svg{
+    width: 64px;
+    height: 64px;
+  }
+}
 .banner {
+  color: #007974;
   padding: 100px 0;
   display: flex;
   justify-content: center;
@@ -36,13 +56,14 @@ name: "Home",
     padding: 8px 0;
     a {
       margin:0 8px;
-      background: #fff;
+      background: $green;
+      color: white;
       display: inline-block;
-      $h: 28px;
-      height: $h;
-      line-height: $h;
-      border-radius: $h/2;
-      padding: 0 8px;
+      border-radius: $border-radius;
+      padding: 8px 24px;
+      &:hover {
+        text-decoration: none;
+      }
     }
   }
 }
