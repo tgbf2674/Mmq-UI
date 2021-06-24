ed665c51491effaed249f312748a0cc3c80931bd
maqing <136518041@qq.com>
2021-06-24 12:20:09 +0800

    代码优化

--- src/router.ts
+++ src/router.ts
@@ -6,11 +6,10 @@ import buttonDemo from './components/buttonDemo.vue'
 import dialogDemo from './components/dialogDemo.vue'
 import tabsDemo from './components/tabsDemo.vue'
 import docDemo from './components/docDemo.vue'
-import Intro from './views/Intro.vue'
-import GetStarted from './views/GetStarted.vue'
-import Install from './views/Install.vue'
-
+import {h} from 'vue'
+import Markdown from './components/Markdown.vue'
 const history = createWebHashHistory();
+const md= filename => h(Markdown,{path: `../markdown/${filename}.md`,key: filename})
 export const router = createRouter({
   history: history,
   routes: [
@@ -18,9 +17,9 @@ export const router = createRouter({
     {path: '/doc', component: Doc,
       children:[
         {path: '',component: docDemo},
-        {path: 'intro', component: Intro},
-        {path: 'get-started', component: GetStarted},
-        {path: 'install', component: Install},
+        {path: 'intro', component: md('intro')},
+        {path: 'get-started', component: md('get-started')},
+        {path: 'install', component:md('install')},
         {path: 'switch',component: switchDemo},
         {path: 'button',component: buttonDemo},
         {path: 'dialog',component: dialogDemo},
