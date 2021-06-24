9ac3a31ccdbf86e27af496dd0d07adb0fa8b11c9
maqing <136518041@qq.com>
2021-06-24 18:30:17 +0800

    添加一键部署

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
-rm -rf dist
-yarn build
-git init
-git add .
-git commit -m 'fc'
-git branch -M master
-git remote add origin git@github.com:tgbf2674/vue3-ui-website.git
-git push -f -u origin master
+rm -rf dist &&
+yarn build &&
+git init &&
+git add . &&
+git commit -m 'fc' &&
+git branch -M master &&
+git remote add origin git@github.com:tgbf2674/vue3-ui-website.git &&
+git push -f -u origin master &&
 cd ..
