183637a0197c426a8312445d572f12e01540e505
maqing <136518041@qq.com>
2021-06-24 17:42:14 +0800

    改造Button Demo

--- src/components/switchDemo.vue
+++ src/components/switchDemo.vue
@@ -7,27 +7,17 @@
 </template>
 
 <script lang="ts">
-import Switch from '../lib/Switch.vue'
-import Button from '../lib/Button.vue';
 import Switch1Demo from './Switch1.demo.vue'
 import Switch2Demo from './Switch2.demo.vue'
 import Demo from './Demo.vue';
-import 'prismjs'
-import 'prismjs/themes/prism-okaidia.css'
-import {ref} from 'vue';
-const Prism = (window as any).Prism
 export default {
 name: "switchDemo",
-  components:{
-  Switch,Button,Demo
+  components:{Demo
   },
   setup(){
-  const bool = ref<boolean>(false)
     return {
-      bool,
       Switch1Demo,
       Switch2Demo,
-      Prism
     }
   }
 }
