f49aa06d7628002dd16ebb1c4ac969c6ca65f13a
maqing <136518041@qq.com>
2021-08-27 15:26:56 +0800

    fc

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ git init &&
 git add . &&
 git commit -m 'fc' &&
 git branch -M master
-git remote add origin git@github.com:tgbf2674/vue3-ui-website.git
+git remote add origin git@github.com:tgbf2674/Vue3UI-website.git
 git push -f -u origin master
 cd ..
