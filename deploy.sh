112ae37a27f643b8095cc45448f9aad77ffe08c7
maqing <136518041@qq.com>
2021-08-27 15:30:42 +0800

    fc

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ yarn build &&
 git init &&
 git add . &&
 git commit -m 'fc' &&
-git branch -M master
-git remote add origin git@github.com:tgbf2674/Vue3UI-website.git
-git push -f -u origin master
+git branch -M master &&
+git remote add origin git@github.com:tgbf2674/Vue3UI-website.git &&
+git push -f -u origin master &&
 cd ..
