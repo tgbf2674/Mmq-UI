9f6a4c60e340b7755e6fc7775e9744b993732bbc
maqing <136518041@qq.com>
2021-08-31 19:37:22 +0800

    sticky component finish

--- src/components/demoInstance/Sticky1.demo.vue
+++ src/components/demoInstance/Sticky1.demo.vue
@@ -1,13 +1,65 @@
 <template>
-
+  <div>
+    <Sticky :distance="20">
+      <div style="width: 100%;height: 100px;background: #3f51b5"></div>
+    </Sticky>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+  </div>
 </template>
 
-<script>
+<script lang="ts">
+import Sticky from '../../lib/Sticky.vue';
+
 export default {
-name: "Sticky1.demo"
-}
+  name: 'Sticky1.demo',
+  components: {Sticky},
+};
 </script>
 
-<style scoped>
-
-</style>
--- src/lib/Sticky.vue
+++ src/lib/Sticky.vue
@@ -1,13 +1,62 @@
 <template>
-
+  <div class="gulu-sticky-wrapper">
+    <div class="gulu-sticky" ref="stickyRef" :class="{sticky}">
+      <slot></slot>
+    </div>
+  </div>
 </template>
 
-<script>
+<script lang="ts">
+import {onMounted, onUnmounted, ref} from 'vue';
+
 export default {
-  name: 'Sticky'
+  name: 'Sticky',
+  props: {
+    distance:{
+      type:Number,
+      default: 0
+    }
+  },
+  setup(props){
+    const sticky = ref(false)
+    const top = ref(0)
+    const stickyRef = ref(null)
+    const handleScroll = () => {
+      if (window.scrollY > top.value - props.distance) {
+        let {width, height, left} = stickyRef.value.getBoundingClientRect()
+        stickyRef.value.style.width = width + 'px'
+        stickyRef.value.style.height = height + 'px'
+        stickyRef.value.style.top = props.distance + 'px'
+        stickyRef.value.style.left = left + 'px'
+        sticky.value = true
+      } else {
+        sticky.value = false
+      }
+    }
+    onMounted(() => {
+      top.value = stickyRef.value.getBoundingClientRect().top
+      window.addEventListener('scroll', handleScroll)
+    })
+    onUnmounted(() => {
+      window.removeEventListener('scroll', handleScroll)
+    })
+    return {sticky, top, stickyRef}
+  }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.gulu-sticky-wrapper {
+  width: 100%;
+  .gulu-sticky {
+    width: 100%;
+    height: 100%;
+    &.sticky {
+      position: fixed;
+      top: 0;
+      left: 0;
+      z-index: 100;
+    }
+  }
+}
 </style>
