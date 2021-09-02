5fe992efc3869328cf5cf914a3773917cce4c234
maqing <136518041@qq.com>
2021-09-02 15:39:25 +0800

    image component init

---
+++ src/components/demo/ImageDemo.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "ImageDemo"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/demoInstance/Image1.demo.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Image1.demo"
+}
+</script>
+
+<style scoped>
+
+</style>
