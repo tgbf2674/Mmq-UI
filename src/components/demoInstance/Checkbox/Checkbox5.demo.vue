976b5750819314f8bf5a2110561965533d503a11
maqing <136518041@qq.com>
2022-10-05 22:46:13 +0800

    update checkbox demo

---
+++ src/components/demoInstance/Checkbox/Checkbox5.demo.vue
@@ -0,0 +1,29 @@
+<demo>
+可选项目数量的限制
+</demo>
+<template>
+  <CheckboxGroup v-model="checkedCities" :min="1" :max="2">
+    <checkbox v-for="city in cities" :key="city" :label="city"></checkbox>
+  </CheckboxGroup>
+</template>
+
+<script lang="ts">
+import checkbox from '../../../lib/Checkbox.vue';
+import CheckboxGroup from '../../../lib/CheckboxGroup.vue';
+import {ref} from 'vue';
+export default {
+  name: 'Checkbox1.demo',
+  components: {checkbox, CheckboxGroup},
+  setup() {
+    const checkedCities = ref(['Shanghai', 'Beijing'])
+    const cities = ['Shanghai', 'Beijing', 'Guangzhou', 'Shenzhen']
+    return {
+      checkedCities, cities
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
