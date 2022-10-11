b0d5e9618ab20c90afbca96888fa8e30e26dc26f
maqing <136518041@qq.com>
2022-10-12 00:46:58 +0800

    update pagination components

--- src/components/demoInstance/Pagination/Pagination1.demo.vue
+++ src/components/demoInstance/Pagination/Pagination1.demo.vue
@@ -2,15 +2,29 @@
  常规使用
 </demo>
 <template>
-  <MqPagination></MqPagination>
+  <MqPagination v-model="current" @changePage="changePage" :page-size="10" :total="80" :page="1" position="center"></MqPagination>
+  {{ current }}
 </template>
 
 <script lang="ts">
 import MqPagination from '../../../lib/MqPagination.vue'
+import {reactive, ref} from 'vue';
 export default {
   name: 'Pagination1.demo',
   components: {
     MqPagination
+  },
+  setup () {
+    const params = reactive({
+      page: 1,
+      pageSize: 10
+    })
+    const current = ref(1)
+    const changePage = (page: number) => {
+      params.page = page
+      console.log(params)
+    }
+    return { changePage, params, current }
   }
 }
 </script>
--- src/lib/MqPagination.vue
+++ src/lib/MqPagination.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="mq-pagination">
-    <a href="javascript:;">上一页</a>
-    <span>...</span>
-    <a href="javascript:;"></a>
-    <span>...</span>
-    <a href="javascript:;">下一页</a>
+  <div class="mq-pagination" :style="{'justify-content': computedPosition}">
+    <a @click="changePage(false)" href="javascript:;" :class="{'disabled': currentPage === 1}">上一页</a>
+    <span v-if="currentPage>3">...</span>
+    <a @click="changePage(item)" href="javascript:;" v-for="item in list" :key="item" :class="{active: currentPage === item}">{{item}}</a>
+    <span v-if="currentPage < pages - 2">...</span>
+    <a @click="changePage(true)" href="javascript:;" :class="{disabled: currentPage === pages}">下一页</a>
   </div>
 </template>
 
 <script>
-import {defineComponent} from 'vue'
+import {computed, defineComponent, ref} from 'vue'
 
 export default defineComponent({
   name: 'MqPagination',
@@ -21,9 +21,59 @@ export default defineComponent({
     pageSize: {
       type: Number,
       default: 10
+    },
+    position: {
+      type: String,
+      default: 'center'
     }
   },
   setup(props, context) {
+    const computedPosition = computed(() => {
+      if (props.position === 'left') return 'flex-start'
+      else if (props.position === 'right') return 'flex-end'
+      else if (props.position === 'center') return 'center'
+    })
+    const pages = computed(() => Math.ceil(props.total / props.pageSize))
+    const currentPage = ref(context.attrs.page || 1)
+    const list = computed(() => {
+      const res = []
+      if (pages.value <= 5) {
+        for (let i = 1;i <= pages.value; i++) {
+          res.push(i)
+        }
+      } else {
+        if (currentPage.value <= 2) {
+          for (let i = 1;i<=5;i++) res.push(i)
+        } else if (currentPage.value >= pages.value - 1) {
+          for (let i = pages.value - 4; i <= currentPage.value + 2; i++) {
+            res.push(i)
+          }
+        } else {
+          for (let i = currentPage.value - 2; i <= currentPage.value + 2; i++) {
+            res.push(i)
+          }
+        }
+      }
+      return res
+    })
+    const changePage = (type) => {
+      if (type === false) {
+        if (currentPage.value === 1)return
+        if (currentPage.value > 1)currentPage.value-= 1
+      } else if (type === true) {
+        if (currentPage.value === pages.value) return
+        if (currentPage.value < pages.value) {
+          currentPage.value += 1
+        }
+      }else {
+        currentPage.value = type
+      }
+      context.emit('changePage', currentPage.value)
+      context.emit('update:modelValue', currentPage.value)
+    }
+    return {
+      computedPosition, currentPage, pages, list, changePage
+    }
   }
 })
 </script>
@@ -31,7 +81,6 @@ export default defineComponent({
 <style lang="scss" scoped>
 .mq-pagination {
   display: flex;
-  justify-content: center;
   padding: 30px;
   > a {
     display: inline-block;
