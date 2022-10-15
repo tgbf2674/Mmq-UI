0d447118c4cabfa5de28d0d72266f114a678a7f3
maqing <136518041@qq.com>
2022-10-15 22:20:59 +0800

    switch component rewrite init

---
+++ src/lib/MqSwitch.vue
@@ -0,0 +1,45 @@
+<template>
+  <div :class="['mmq-switch-wrapper']" @click="changeSwitchStatus">
+    <div :class="['mmq-switch-status', switchStatusClass]">
+      <span class="mmq-switch-text">{{ switchStatusText }}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  name: 'MqSwitch',
+  props: {
+    checked: {
+      required: true,
+      default: false,
+      type: Boolean
+    },
+    defaultChecked: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    openText: {
+      type: String,
+      default: ''
+    },
+    closeText: {
+      type: String,
+      default: ''
+    }
+  },
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
