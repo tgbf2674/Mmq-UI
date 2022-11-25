77c5aafea3295ad5f69762008d47a9a53f9c1736
maqing <136518041@qq.com>
2022-11-25 23:25:22 +0800

    update tag test

--- src/tests/tag.test.js
+++ src/tests/tag.test.js
@@ -6,9 +6,21 @@ describe('tag组件', () => {
   test('基本用法', () => {
     const wrapper = mount(Tag, {
       props: {
+        name: '标签一',
         type: 'success'
       }
     })
     expect(wrapper.classes('mmq-tag-success')).toBe(true)
+  })
+  test('可移除标签按钮', async () => {
+    const wrapper = mount(Tag, {
+      props: {
+        name: '标签二',
+        type: 'success',
+        canClose: true
+      }
+    })
+    await wrapper.find('.close').trigger('click')
+    expect(wrapper.find('.close').exists()).toBe(true)
   })
 })
