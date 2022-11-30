a9eab29d4c13c01af39c2e44b32e6b7448f1cc85
maqing <136518041@qq.com>
2022-11-30 23:02:27 +0800

    update skeleton test

--- src/tests/skeleton.test.js
+++ src/tests/skeleton.test.js
@@ -33,4 +33,26 @@ describe('Skeleton组件', () => {
     const wrapper = mount(Component)
     expect(wrapper.find('.is-animated').exists()).toBe(true)
   })
+  test('自定义格式', () => {
+    const Component = {
+      template: `
+        <skeleton style="width:240px">
+        <template #template>
+          <skeletonItem variant="image" style="width: 240px; height: 240px">
+          </skeletonItem>
+          <div style="padding: 14px">
+            <skeletonItem variant="p" style="width: 50%"></skeletonItem>
+            <div style="display: flex;align-items: center;justify-content: space-between">
+              <skeletonItem variant="text" style="margin-right: 16px"></skeletonItem>
+              <skeletonItem variant="text" style="width: 30%"></skeletonItem>
+            </div>
+          </div>
+        </template>
+        </skeleton>
+  `,
+      components: {Skeleton, SkeletonItem}
+    }
+    const wrapper = mount(Component)
+    expect(wrapper.find('.mmq-skeleton-item').exists()).toBe(true)
+  })
 })
