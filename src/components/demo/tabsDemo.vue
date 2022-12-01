6fe8cd5c245dd8581e4e8098f8708c331e146262
maqing <136518041@qq.com>
2022-12-02 02:49:59 +0800

    update tabs component demo

--- src/components/demo/tabsDemo.vue
+++ src/components/demo/tabsDemo.vue
@@ -2,6 +2,7 @@
   <h1>Tabs 示例</h1>
   <Demo :component="Tabs1Demo" />
   <Demo :component="Tabs2Demo" />
+  <Demo :component="Tabs3Demo" />
 </template>
 
 <script>
@@ -10,6 +11,7 @@ import Tab from '../../lib/Tab.vue'
 import Demo from '../Demo.vue'
 import Tabs1Demo from '../demoInstance/Tab/Tabs1.demo.vue'
 import Tabs2Demo from '../demoInstance/Tab/Tabs2.demo.vue'
+import Tabs3Demo from '../demoInstance/Tab/Tabs3.demo.vue'
 export default {
 name: "tabsDemo",
   components: {
@@ -17,7 +19,7 @@ name: "tabsDemo",
   },
   setup(){
     return {
-      Tabs1Demo, Tabs2Demo
+      Tabs1Demo, Tabs2Demo, Tabs3Demo
     }
   }
 }
---
+++ src/components/demoInstance/Tab/Tabs3.demo.vue
@@ -0,0 +1,29 @@
+<demo>
+带有边框的卡片风格
+</demo>
+<template>
+  <Tabs type="border-card" v-model="x">
+    <Tab title="导航1">内容1</Tab>
+    <Tab title="导航2">内容2</Tab>
+  </Tabs>
+</template>
+
+<script lang="ts">
+import Tabs from '../../../lib/Tabs.vue'
+import Tab from '../../../lib/Tab.vue'
+import {
+  ref
+} from 'vue'
+export default {
+  components: {
+    Tabs,
+    Tab
+  },
+  setup() {
+    const x = ref('导航2')
+    return {
+      x
+    }
+  }
+}
+</script>
