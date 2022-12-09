c775da42d64d15722d239d64e3787f7ff969d20e
maqing <136518041@qq.com>
2022-12-09 22:33:39 +0800

    update tooltip test

--- src/tests/tooltip.test.js
+++ src/tests/tooltip.test.js
@@ -1,6 +1,7 @@
 import {mount} from '@vue/test-utils'
 import {describe, test, expect} from 'vitest'
 import Tooltip from '../lib/Tooltip.vue'
+import Button from '../lib/Button.vue'
 describe('Tooltip组件', () => {
   test('基本使用', async () => {
     const Component = {
@@ -9,9 +10,21 @@ describe('Tooltip组件', () => {
         <Button>右边</Button>
         </Tooltip>
   `,
-      components: {Tooltip}
+      components: {Tooltip, Button}
     }
     const wrapper = mount(Component)
     expect(wrapper.find('.tooltip').exists()).toBe(true)
+  })
+  test('切换主题', async () => {
+    const Component = {
+      template: `
+        <Tooltip content="我是出现在右边的文字" effect="light" direction="right">
+        <Button>右边</Button>
+        </Tooltip>
+  `,
+      components: {Tooltip, Button}
+    }
+    const wrapper = mount(Component)
+    expect(wrapper.find('.tooltipText').element.style.background).toBe('#fff')
   })
 })
