72985cdfa1c0658add600797e383c0fc01c30874
maqing <136518041@qq.com>
2022-12-12 03:21:28 +0800

    update inputNumber components

--- src/lib/InputNumber.vue
+++ src/lib/InputNumber.vue
@@ -1,15 +1,23 @@
 <template>
-  <span ref="inputNumberRef" :class="['mmq-inputNumber', disabled && 'disabled']">
-    <div :class="['mmq-inputNumber-decrease']" @click.prevent="handleButtonMinus"><Icon name="icon-minus"></Icon></div>
+  <div ref="inputNumberRef" :class="['mmq-inputNumber', disabled && 'disabled']">
+    <div :style="MinusNotAllowedStyle">
+      <div :style="MinusPointerNoneStyle" :class="['mmq-inputNumber-decrease']" @click.prevent="handleButtonMinus">
+        <Icon name="icon-minus"></Icon>
+      </div>
+    </div>
     <Input :disabled="disabled" placeholder v-model:input-value="inputValue" @input="handleInputValue"/>
-    <div class="mmq-inputNumber-increase" @click.stop="handleButtonAdd"><Icon name="icon-add"></Icon></div>
-  </span>
+    <div :style="IncreaseNotAllowedStyle">
+      <div :style="IncreasePointerNoneStyle" class="mmq-inputNumber-increase" @click.stop="handleButtonAdd">
+        <Icon name="icon-add"></Icon>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import Input from './Input.vue'
 import Icon from './Icon.vue'
-import {defineComponent, watchEffect, ref, onMounted} from 'vue'
+import {defineComponent, watchEffect, ref, onMounted, watch, computed} from 'vue'
 
 export default defineComponent({
   name: 'InputNumber',
@@ -22,22 +30,55 @@ export default defineComponent({
     disabled: {
       type: Boolean,
       default: false
+    },
+    min: {
+      type: Number,
+      default: -Infinity
+    },
+    max: {
+      type: Number,
+      default: +Infinity
     }
   },
   setup(props, context) {
     const inputValue = ref()
     const inputNumberRef = ref()
+    const canMinusClick = () => {
+      return !props.disabled && inputValue.value > props.min
+    }
+    const canIncreaseClick = () => {
+      return !props.disabled && inputValue.value < props.max
+    }
+    const MinusNotAllowedStyle = computed(() => {
+      if (!canMinusClick()) return {
+        cursor: 'not-Allowed'
+      }
+    })
+    const MinusPointerNoneStyle = computed(() => {
+      if (!canMinusClick()) return {
+        pointerEvents: 'none'
+      }
+    })
+    const IncreaseNotAllowedStyle = computed(() => {
+      if (!canIncreaseClick()) return {
+        cursor: 'not-Allowed'
+      }
+    })
+    const IncreasePointerNoneStyle = computed(() => {
+      if (!canIncreaseClick()) return {
+        pointerEvents: 'none'
+      }
+    })
     watchEffect(() => {
       inputValue.value = props.modelValue
     })
-    onMounted(() => {
-      if (props.disabled) {
-        inputNumberRef.value.children[0].style.pointerEvents = 'none'
-        inputNumberRef.value.children[2].style.pointerEvents = 'none'
-      }
-    })
     const handleButtonMinus = () => {
-      context.emit('update:modelValue', props.modelValue - 1)
+      const curVal = props.modelValue - 1
+      if (curVal < props.min) {
+        context.emit('update:modelValue', props.min)
+      } else {
+        context.emit('update:modelValue', curVal)
+      }
     }
     const handleButtonAdd = () => {
       context.emit('update:modelValue', props.modelValue + 1)
@@ -47,7 +88,17 @@ export default defineComponent({
       value ? context.emit('update:modelValue', value) : context.emit('update:modelValue', 0)
     }
     return {
-      handleButtonAdd, handleButtonMinus, handleInputValue, inputValue, inputNumberRef
+      handleButtonAdd,
+      handleButtonMinus,
+      handleInputValue,
+      inputValue,
+      inputNumberRef,
+      canMinusClick,
+      canIncreaseClick,
+      MinusNotAllowedStyle,
+      MinusPointerNoneStyle,
+      IncreaseNotAllowedStyle,
+      IncreasePointerNoneStyle
     }
   }
 })
@@ -121,11 +172,14 @@ export default defineComponent({
 
 .disabled {
   cursor: not-allowed;
+
   &:hover {
     border: none;
   }
+
   ::v-deep .mmq-input-inner {
     cursor: not-allowed;
+
     &:hover {
       border-color: #dcdfe6;
     }
