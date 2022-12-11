3a5876bebc027452a6027caedcfb9c6b73c75ca7
maqing <136518041@qq.com>
2022-12-12 03:32:04 +0800

    update doc

--- src/components/demo/InputNumberDemo.vue
+++ src/components/demo/InputNumberDemo.vue
@@ -2,16 +2,18 @@
   <h1>InputNumber示例</h1>
   <Demo :component="InputNumber1"/>
   <Demo :component="InputNumber2"/>
+  <Demo :component="InputNumber3"/>
 </template>
 
 <script lang="ts">
 import Demo from '../Demo.vue';
 import InputNumber1 from '../demoInstance/InputNumber/InputNumber1.demo.vue'
 import InputNumber2 from '../demoInstance/InputNumber/InputNumber2.demo.vue'
+import InputNumber3 from '../demoInstance/InputNumber/InputNumber3.demo.vue'
 export default {
   components: {Demo},
   setup() {
-    return {InputNumber1, InputNumber2};
+    return {InputNumber1, InputNumber2, InputNumber3};
   }
 };
 </script>
---
+++ src/components/demoInstance/InputNumber/InputNumber3.demo.vue
@@ -0,0 +1,20 @@
+<demo>
+最大值最小值限制
+</demo>
+
+<template>
+  <h5>max,min属性接受一个 Number，设定值的范围。</h5>
+  <InputNumber v-model="num"/>
+</template>
+
+<script lang="ts">
+import InputNumber from "../../../lib/InputNumber.vue";
+import {ref} from 'vue';
+export default {
+  components: {InputNumber},
+  setup() {
+    const num = ref(1);
+    return {num};
+  }
+};
+</script>
