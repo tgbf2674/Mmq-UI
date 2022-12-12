aa3df0765e16d34c447a222e4143adf7125b1716
maqing <136518041@qq.com>
2022-12-13 03:04:55 +0800

    replace new Icon component

--- src/lib/Icon.vue
+++ src/lib/Icon.vue
@@ -9,7 +9,7 @@
 <script lang="ts">
 import {defineComponent} from 'vue';
 export default defineComponent({
-  name: "MqIcon",
+  name: "Icon",
   props: {
     name: {
       type: String,
--- src/lib/MqIcon.vue
+++ src/lib/MqIcon.vue
@@ -9,7 +9,7 @@
 <script lang="ts">
 import {defineComponent} from 'vue';
 export default defineComponent({
-  name: "MqIcon",
+  name: "MqIcons",
   props: {
     name: {
       type: String,
