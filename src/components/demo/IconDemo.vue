1bbf40ca8a8788127d66db5626dd954282bd6e47
maqing <136518041@qq.com>
2022-12-13 20:06:19 +0800

    update doc

--- src/components/demo/IconDemo.vue
+++ src/components/demo/IconDemo.vue
@@ -1,5 +1,6 @@
 <template>
   <h1>Icon图标</h1>
+  <quickUse />
   <Demo :component="Icon1Demo"/>
   <Demo :component="Icon2Demo"/>
   <Demo :component="IconList"/>
@@ -7,11 +8,12 @@
 
 <script lang="ts">
 import Demo from '../Demo.vue';
+import quickUse from '../demoInstance/Icon/quickUse.vue'
 import Icon1Demo from '../demoInstance/Icon/Icon1.demo.vue'
 import Icon2Demo from '../demoInstance/Icon/Icon2.demo.vue'
 import IconList from '../demoInstance/Icon/IconList.demo.vue';
 export default {
-  components: {Demo},
+  components: {Demo, quickUse},
   setup() {
     return {Icon1Demo, Icon2Demo, IconList};
   }
---
+++ src/components/demoInstance/Icon/quickUse.vue
@@ -0,0 +1,51 @@
+<template>
+  <div style="margin: 24px 0">
+    <h3>1.安装图标库</h3>
+    <div class="InstallWrapper">
+      <pre>
+        <code>
+      <span># NPM</span>
+      <span>$ npm install @element-plus/icons-vue</span>
+      <span># Yarn</span>
+      <span>$ yarn add @element-plus/icons-vue</span>
+      <span># pnpm</span>
+      <span>$ pnpm install @element-plus/icons-vue</span>
+          </code>
+      </pre>
+    </div>
+    <h3>2.注册所有图标</h3>
+    <div class="InstallWrapper">
+      <pre>
+        <code>
+          <strong>在main.ts中加入下面的代码,即可直接使用</strong>
+
+          <span>import * as ElementPlusIconsVue from '@element-plus/icons-vue'</span>
+
+          <span>const app = createApp(App)</span>
+          <span>for (const [key, component] of Object.entries(ElementPlusIconsVue)) {</span>
+            <span>app.component(key, component)</span>
+          <span>}</span>
+        </code>
+      </pre>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import MqIcon from '../../../lib/MqIcon.vue';
+
+export default {
+  components: {MqIcon},
+  setup() {
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.InstallWrapper {
+  background-color: #f5f7fa;
+  span {
+    color: #303133;
+  }
+}
+</style>
