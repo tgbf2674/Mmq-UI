308b434d6dbcaf2b265e817fb378821b20e0c2f6
maqing <136518041@qq.com>
2023-01-02 23:54:04 +0800

    form component init

--- src/lib/Form.vue
+++ src/lib/Form.vue
@@ -1,11 +1,22 @@
 <template>
-
+  <form class="mmq-form">
+    <slot></slot>
+  </form>
 </template>
 
-<script>
-export default {
-name: "Form"
-}
+<script lang="ts">
+import {defineComponent, ref} from 'vue';
+
+export default defineComponent({
+  name: 'MqForm',
+  setup() {
+    const field = ref([])
+    const formError = ref({})
+    return {
+      field, formError
+    }
+  }
+})
 </script>
 
 <style scoped>
---
+++ src/lib/FormItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <div>
+      <slot></slot>
+      <p v-if="error">{{ error }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, onMounted, ref} from 'vue';
+
+export default defineComponent({
+  name: 'FormItem',
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    prop: {
+      type: String,
+      default: ''
+    }
+  },
+  setup() {
+    const error = ref('');
+    return {
+      error
+    };
+  }
+});
+</script>
+
+<style scoped>
+
+</style>
