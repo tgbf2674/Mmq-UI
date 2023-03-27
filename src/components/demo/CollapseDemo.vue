0a1aed1ed240a740ee4892ed5f55fd0eac5bcee2
maqing <136518041@qq.com>
2023-03-27 18:04:12 +0800

    update doc

--- src/components/demo/CollapseDemo.vue
+++ src/components/demo/CollapseDemo.vue
@@ -3,75 +3,12 @@
   <Demo :component="Collapse1Demo"></Demo>
   <Demo :component="Collapse2Demo"></Demo>
   <Demo :component="Collapse3Demo"></Demo>
-  <h3 style="margin-top: 40px;">Collapse 事件</h3>
-  <table width="100%">
-    <tr>
-      <th>事件名</th>
-      <th>说明</th>
-      <th>参数</th>
-    </tr>
-    <tr>
-      <td>change</td>
-      <td>当前激活面板改变时触发(如果是手风琴模式，参数 activeNames 类型为string，否则为array)</td>
-      <td>新当前页</td>
-    </tr>
-  </table>
-  <h3 style="margin-top: 40px;">Collapse 属性</h3>
-  <table width="100%">
-    <tr>
-      <th>属性名</th>
-      <th>说明</th>
-      <th>类型</th>
-      <th>可选值</th>
-      <th>默认值</th>
-    </tr>
-    <tr>
-      <td>v-model:activeName</td>
-      <td>当前激活的面板(如果是手风琴模式，绑定值类型需要为string，否则为array)</td>
-      <td>string (accordion mode) / array (non-accordion mode)</td>
-      <td>-</td>
-      <td>-</td>
-    </tr>
-    <tr>
-      <td>accordion</td>
-      <td>是否手风琴模式</td>
-      <td>boolean</td>
-      <td>-</td>
-      <td>false</td>
-    </tr>
-  </table>
-  <h3 style="margin-top: 40px;">Collapse Item属性</h3>
-  <table width="100%">
-    <tr>
-      <th>属性名</th>
-      <th>说明</th>
-      <th>类型</th>
-      <th>可选值</th>
-      <th>默认值</th>
-    </tr>
-    <tr>
-      <td>name</td>
-      <td>唯一标志符</td>
-      <td>string/number</td>
-      <td>-</td>
-      <td>-</td>
-    </tr>
-    <tr>
-      <td>title</td>
-      <td>面板标题</td>
-      <td>string</td>
-      <td>-</td>
-      <td>-</td>
-    </tr>
-    <tr>
-      <td>disabled</td>
-      <td>是否禁用</td>
-      <td>boolean</td>
-      <td>-</td>
-      <td>-</td>
-    </tr>
-  </table>
-
+  <H2>Collapse 事件</H2>
+  <MqTable :columns="columns" :data-source="data" bordered></MqTable>
+  <H2>Collapse 属性</H2>
+  <MqTable :columns="columns1" :data-source="data1" bordered></MqTable>
+  <H2>Collapse Item属性</H2>
+  <MqTable :columns="columns1" :data-source="data1" bordered></MqTable>
 </template>
 
 <script lang="ts">
@@ -79,12 +16,42 @@ import Demo from '../Demo.vue'
 import Collapse1Demo from '../demoInstance/Collapse/Collapse1.demo.vue'
 import Collapse2Demo from '../demoInstance/Collapse/Collapse2.demo.vue'
 import Collapse3Demo from '../demoInstance/Collapse/Collapse3.demo.vue'
+import {reactive} from 'vue';
 export default {
 name: "CollapseDemo",
   components: {Demo},
   setup(){
+    const data = reactive([
+      {key: '1', attr: 'change', describe: '当前激活面板改变时触发(如果是手风琴模式，参数 activeNames 类型为string，否则为array)', params: '新当前页'},
+    ])
+    const columns = reactive([
+      {title: '参数', dataIndex: 'attr', key: 'attr'},
+      {title: '说明', dataIndex: 'describe', key: 'describe'},
+      {title: '参数', dataIndex: 'params', key: 'params'}
+    ])
+    const data1 = reactive([
+      {key: '1', attr: 'v-model:activeName', describe: '当前激活的面板(如果是手风琴模式，绑定值类型需要为string，否则为array)', type: 'string (accordion mode) / array (non-accordion mode)', default: '-'},
+      {key: '2', attr: 'accordion', describe: '是否手风琴模式', type: 'boolean', default: 'false'}
+    ])
+    const columns1 = reactive([
+      {title: '参数', dataIndex: 'attr', key: 'attr'},
+      {title: '说明', dataIndex: 'describe', key: 'describe'},
+      {title: '类型', dataIndex: 'type', key: 'type'},
+      {title: '默认值', dataIndex: 'default', key: 'default'}
+    ])
+    const data2 = reactive([
+      {key: '1', attr: 'name', describe: '唯一标志符', type: 'string/number', default: '-'},
+      {key: '2', attr: 'title', describe: '面板标题', type: 'string', default: '-'},
+      {key: '2', attr: 'disabled', describe: '是否禁用', type: 'boolean', default: '-'}
+    ])
+    const columns2 = reactive([
+      {title: '参数', dataIndex: 'attr', key: 'attr'},
+      {title: '说明', dataIndex: 'describe', key: 'describe'},
+      {title: '类型', dataIndex: 'type', key: 'type'},
+      {title: '默认值', dataIndex: 'default', key: 'default'}
+    ])
   return {
-    Collapse1Demo,Collapse2Demo, Collapse3Demo
+    Collapse1Demo,Collapse2Demo, Collapse3Demo, data, columns, columns1, data1, columns2, data2
   }
   }
 }
--- src/components/demo/PaginationDemo.vue
+++ src/components/demo/PaginationDemo.vue
@@ -3,29 +3,8 @@
   <Demo :component="PaginationDemo1"></Demo>
   <Demo :component="PaginationDemo2"></Demo>
   <Demo :component="PaginationDemo3"></Demo>
-  <h2>事件</h2>
-  <table width="100%">
-    <tr>
-      <th>事件名</th>
-      <th>说明</th>
-      <th>参数</th>
-    </tr>
-    <tr>
-      <td>changePage</td>
-      <td>currentPage 改变时触发</td>
-      <td>新当前页</td>
-    </tr>
-    <tr>
-      <td>prevClick</td>
-      <td>用户点击上一页按钮改变当前页时触发</td>
-      <td>新当前页</td>
-    </tr>
-    <tr>
-      <td>nextClick</td>
-      <td>用户点击下一页按钮改变当前页时触发</td>
-      <td>新当前页</td>
-    </tr>
-  </table>
+  <H2>事件</H2>
+  <MqTable :columns="columns" :data-source="data" bordered></MqTable>
 </template>
 
 <script>
@@ -33,13 +12,24 @@ import Demo from '../Demo.vue'
 import PaginationDemo1 from '../demoInstance/Pagination/Pagination1.demo.vue'
 import PaginationDemo2 from '../demoInstance/Pagination/Pagination2.demo.vue'
 import PaginationDemo3 from '../demoInstance/Pagination/Pagination3.demo.vue'
+import {reactive} from 'vue'
 
 export default {
   name: 'PaginationDemo',
   components: {Demo},
   setup() {
+    const data = reactive([
+      {key: '1', attr: 'changePage', describe: 'currentPage 改变时触发', params: '新当前页'},
+      {key: '2', attr: 'prevClick', describe: '用户点击上一页按钮改变当前页时触发', params: '新当前页'},
+      {key: '3', attr: 'nextClick', describe: '用户点击下一页按钮改变当前页时触发', params: '新当前页'}
+    ])
+    const columns = reactive([
+      {title: '参数', dataIndex: 'attr', key: 'attr'},
+      {title: '说明', dataIndex: 'describe', key: 'describe'},
+      {title: '参数', dataIndex: 'params', key: 'params'}
+    ])
     return {
-      PaginationDemo1, PaginationDemo2, PaginationDemo3
+      PaginationDemo1, PaginationDemo2, PaginationDemo3, data, columns
     }
   }
 }
